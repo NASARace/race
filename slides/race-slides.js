20bba7995a0455ffb83ba59b3bb32ff61e396ddb
peter <pcmehlitz@gmail.com>
2016-09-25 23:24:45 -0700

    update from master doc

--- slides/race-slides.js
+++ slides/race-slides.js
@@ -2,6 +2,14 @@ var slides = document.getElementsByClassName("slide");
 var isFullScreen = false;
 var firstDigit = 0;
 
+var timer = document.getElementById('timer');
+var timerInterval = 5; // in sec
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+var t = 0;
+var timed = false
+
 function toggleFullScreen() {
   if (!isFullScreen){
     var elem = document.documentElement;
@@ -17,11 +25,36 @@ function toggleFullScreen() {
     isFullScreen = false;
     if(document.exitFullscreen) {
       document.exitFullscreen();
-      } else if(document.mozCancelFullScreen) {
+    } else if(document.mozCancelFullScreen) {
       document.mozCancelFullScreen();
-      } else if(document.webkitExitFullscreen) {
+    } else if(document.webkitExitFullscreen) {
       document.webkitExitFullscreen();
     }
+  }
+}
+
+function incTime() {
+  if (timed){
+    t += timerInterval;
+    seconds = t % 60;
+    minutes = ~~(t/60) % 60;
+    hours = ~~(t/3600)
+    timer.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+                        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+                        (seconds > 9 ? seconds : "0" + seconds);
+
+    setTimeout(incTime,timerInterval*1000);
+  }
+}
+
+function toggleTimer() {
+  if (timed){
+    timed = false;
+    timer.style.color = '#0000FF';
+  } else {
+    timed = true;
+    timer.style.color = '#FF0000';
+    setTimeout(incTime,timerInterval*1000);
   }
 }
 
@@ -78,14 +111,18 @@ function targetSlideIndex (delta){
 }
 
 document.onkeypress = function (e) {
-  if (e.which == 13){  // Enter: next slide, Shift+Enter: prev slide
+  var kc = e.which
+  if (kc == 13){  // Enter: next slide, Shift+Enter: prev slide
     scrollToSlide( targetSlideIndex(e.shiftKey ? -1 : 1));
   }
-  else if (e.which == 102) { // 'f' toggle full screen
+  else if (kc == 102) { // 'f' toggle full screen
     toggleFullScreen();
   }
-  else if (e.which >=48 && e.which <= 57){ // jump to slide 0..9
-    var idx = e.which - 48;
+  else if (kc == 116){  // 't' toggles timer
+    toggleTimer();
+  }
+  else if (kc >=48 && kc <= 57){ // jump to slide 0..9
+    var idx = kc - 48;
     if (e.ctrlKey){
       firstDigit = idx;
     } else {
