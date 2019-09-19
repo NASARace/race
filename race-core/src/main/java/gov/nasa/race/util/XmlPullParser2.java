f660689aefea582b9f280207494d5903d01d9c78
Peter Mehlitz <Peter.C.Mehlitz>
2019-09-19 16:40:20 -0700

    unfortunately we also need to move RangeStack over to Java since we don't want to call Scala from Java (which is lower level)

--- race-core/src/main/java/gov/nasa/race/util/XmlPullParser2.java
+++ race-core/src/main/java/gov/nasa/race/util/XmlPullParser2.java
@@ -16,5 +16,53 @@
  */
 package gov.nasa.race.util;
 
+/**
+ * XmlParser that is based on Slices instead of Strings to avoid temporary objects
+ */
 public class XmlPullParser2 {
+
+    static final int STATE_TAG = 1;
+    static final int STATE_ATTR = 2;
+    static final int STATE_END_TAG = 3;
+    static final int STATE_CONTENT = 4;
+    static final int STATE_FINISHED = 5;
+
+    protected byte[] data; // the utf-8 bytes we are parsing
+    protected int idx = 0; // points to the next un-parsed position in data
+
+    protected int state = STATE_TAG;
+
+    protected RangeStack path = new RangeStack(32); // the element path
+
+    public Slice tag = new Slice(data,0,0);
+    public boolean isStartTag = false;
+    protected boolean wasEmptyElementTag = false;
+
+    public Slice attrName = new Slice(data,0,0);
+    public Slice attrValue = new Slice(data,0,0);
+
+    protected RangeStack contentStrings = new RangeStack(8);
+    protected int contentIdx = 0;
+
+    //--- the public API
+
+    public boolean parseNextTag() {
+        return false;
+    }
+
+    public boolean parseNextAttr() {
+        return false;
+    }
+
+    public boolean parseAttr (Slice attr) {
+        return false;
+    }
+
+    public boolean parseContent() {
+        return false;
+    }
+
+    //--- internal helpers
+
+
 }
