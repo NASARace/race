7f6662de68567bad9ed90459aa2d8aedc9948fff
Peter Mehlitz <Peter.C.Mehlitz>
2019-09-26 15:51:18 -0700

    added a explicit token tree branching version, which can save up to 10% compared to slice comparison

--- race-core-test/src/main/scala/gov/nasa/race/common/XmlPullParser2Benchmark.scala
+++ race-core-test/src/main/scala/gov/nasa/race/common/XmlPullParser2Benchmark.scala
@@ -23,6 +23,9 @@ object XmlPullParser2Benchmark {
 
     prompt("\n---- hit any key to run new parser..")
     runXmlPullParser2(msg)
+
+    prompt("\n---- hit any key to run new tree parser..")
+    runXmlPullParser2Tree(msg)
   }
 
   def prompt (msg: String): Unit = {
@@ -123,6 +126,87 @@ object XmlPullParser2Benchmark {
       if (parser.initialize(msg)) {
         while (parser.parseNextTag) {
           if (parser.tag == flight && parser.isStartTag) parseFlight
+        }
+      }
+    }
+  }
+
+  def runXmlPullParser2Tree (msg: Array[Byte]): Unit = {
+    println(s"-- parsing ${nRounds}x using XmlPullParser2 tree branching")
+    val parser = new UTF8XmlPullParser2
+
+    val slicer = new SliceSplitter(' ')
+    var nFlights = 0
+
+    //--- attributes and ancestor elements
+    val aircraftIdentification = Slice("aircraftIdentification")
+    val enRoute = Slice("enRoute")
+    val location = Slice("location")
+    val positionTime = Slice("positionTime")
+    val flight = Slice("flight")
+
+    def parseFlight: Unit = {
+      // here we can keep cache vars at the terminal level, i.e. don't have to reset
+      var id: String = null
+      var lat, lon: Double = 0.0
+      var dtg: String = null
+
+      while (parser.parseNextTag) {
+        val tag = parser.tag
+        val data = tag.data
+        val i = tag.offset
+        val len = tag.length
+
+        if (parser.isStartTag) {
+          if (len > 0) {
+            if (data(i) == 'f'){ // flightIdentification
+              if (len == 20 && data(i+1)=='l' && data(i+2)=='i' && data(i+3)=='g' && data(i+4)=='h' && data(i+5)=='t' && data(i+6)=='I' && data(i+7)=='d' && data(i+8)=='e' && data(i+9)=='n' && data(i+10)=='t' && data(i+11)=='i' && data(i+12)=='f' && data(i+13)=='i' && data(i+14)=='c' && data(i+15)=='a' && data(i+16)=='t' && data(i+17)=='i' && data(i+18)=='o' && data(i+19)=='n'){
+                if (parser.parseAttr(aircraftIdentification)) id = parser.attrValue.intern
+              }
+            } else if (data(i) == 'p') { // position | pos
+              if ((len > 2) && (data(i+1) == 'o') && (data(i+2) == 's')) {
+                if (len == 3) { // pos
+                  if (parser.tagHasParent(location)) {
+                    if (parser.parseContent && parser.getNextContentString) {
+                      slicer.setSource(parser.contentString)
+                      if (slicer.hasNext) lat = slicer.next.toDouble
+                      if (slicer.hasNext) lon = slicer.next.toDouble
+                    }
+                  }
+                } else if (len == 8 && data(i + 3) == 'i' && data(i + 4) == 't' && data(i + 5) == 'i' && data(i + 6) == 'o' && data(i + 7) == 'n') {
+                  if (parser.tagHasParent(enRoute)) {
+                    if (parser.parseAttr(positionTime)) dtg = parser.attrValue.toString
+                  }
+                }
+              }
+            }
+          }
+
+        } else { // end tag
+          if (len > 0){
+            if (data(i) == 'f') { // /flight
+              if ((len == 6) && data(i+1)=='l' && data(i+2)=='i' && data(i+3)=='g' && data(i+4)=='h' && data(i+5)=='t') {
+                if (id != null && lat != 0.0 && lon != 0.0 && dtg != null) {
+                  nFlights += 1
+                  //println(s"$nFlights: $id, $dtg, $lat, $lon")
+                  return
+                }
+              }
+            } else if (data(i) == 'm') { // messageCollection
+              if ((len==17) && data(i+1)=='e' && data(i+2)=='s' && data(i+3)=='s' && data(i+4)=='a' && data(i+5)=='g' && data(i+6)=='e' && data(i+7)=='C' && data(i+8)=='o' && data(i+9)=='l' && data(i+10)=='l' && data(i+11)=='e' && data(i+12)=='c' && data(i+13)=='t' && data(i+14)=='i' && data(i+15)=='o' && data(i+16)=='n'){
+                assert( nFlights == 100)
+                nFlights = 0
+              }
+            }
+          }
+        }
+      }
+    }
+
+    measure(nRounds) {
+      if (parser.initialize(msg)) {
+        while (parser.parseNextTag) {
+          if (parser.isStartTag && parser.tag == flight) parseFlight
         }
       }
     }
