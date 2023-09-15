b5881acd9cae98036f91869b66be30ac67b61606
Peter Mehlitz <pcmehlitz@gmail.com>
2023-09-14 23:55:09 -0700

    fixed canvas framebuffer color probe. Apparently the format changed to gl.RGBA (was affecting colormap lookup)
    fixed KvTable (wrong data-rows attribute value)

--- race-earth/src/main/rust/odin_dem/src/lib.rs
+++ race-earth/src/main/rust/odin_dem/src/lib.rs
@@ -1,2 +1,39 @@
 #![allow(unused)]
 
+use std::error::Error;
+use std::fs::File;
+use std::path::PathBuf;
+
+use anyhow::{anyhow,Result};
+
+pub struct BoundingBox {
+    west: f64,
+    south: f64,
+    east: f64,
+    north: f64
+}
+
+pub enum DemImgType {
+    PNG,
+    TIF,
+}
+
+impl DemImgType {
+    fn file_extension(&self) -> &'static str {
+        match *self {
+            DemImgType::PNG => "png",
+            DemImgType::TIF => "tif",
+        }
+    }
+}
+
+
+fn get_filename (bbox: &BoundingBox, precision: usize, file_ext: &str) -> String {
+    format!("dem_{:.precision$},{:.precision$},{:.precision$},{:.precision$}.{}",
+            bbox.west, bbox.south, bbox.east, bbox.north, file_ext)
+}
+
+pub fn get_dem (bbox: &BoundingBox, epsg_srs: u32, img_type: DemImgType, cache_dir: &str) -> Result<File> {
+
+    Err(anyhow!("failed to retrieve DEM"))
+}
