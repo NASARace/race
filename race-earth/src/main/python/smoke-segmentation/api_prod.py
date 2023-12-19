ba409eb269e6bf7e0502b497b2893bd2aceda332
Sequoia Andrade <sequoia.r.andrade@nasa.gov>
2023-12-19 13:55:17 -0800

    updated files to use huggingface model, added liscense

--- race-earth/src/main/python/smoke-segmentation/api_prod.py
+++ race-earth/src/main/python/smoke-segmentation/api_prod.py
@@ -1,4 +1,20 @@
-import zipfile
+# /*
+#  * Copyright (c) 2022, United States Government, as represented by the
+#  * Administrator of the National Aeronautics and Space Administration.
+#  * All rights reserved.
+#  *
+#  * The RACE - Runtime for Airspace Concept Evaluation platform is licensed
+#  * under the Apache License, Version 2.0 (the "License"); you may not use
+#  * this file except in compliance with the License. You may obtain a copy
+#  * of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+#  */
+
 import tensorflow as tf
 from flask import Flask, request, jsonify, abort, redirect, send_file, Response
 from werkzeug.utils import secure_filename
@@ -12,8 +28,8 @@ import uuid
 import logging
 import time
 import numpy as np
-import psutil
 from keras import backend as K
+from huggingface_hub import from_pretrained_keras
 
 os.environ["USE_PATH_FOR_GDAL_PYTHON"]="NO"
 
@@ -59,11 +75,10 @@ def remove_upload_files():
     shutil.rmtree(UPLOAD_FOLDER)
 
 def load_model():
-    model_path = os.path.join(os.getcwd(), 'models/final_BT_unet.h5')
-    #model_path = os.path.join(os.getcwd(), "models", 'BT_unet_T')
+    #model_path = os.path.join(os.getcwd(), 'models/final_BT_unet.h5')
     global segmentation_model
-    #print(model_path)
-    segmentation_model = initiate_model(model_path)
+    segmentation_model = from_pretrained_keras("sequoiaandrade/Smoke-Cloud-Segmentation-RACE-ODIN")
+    #segmentation_model = initiate_model(model_path)
 
 # route definition
 
